e4e3e2728311cde1c4f8f5e1085118887201b9b9
Huy Nguyen Huu <huuhuy@gmail.com>
2015-12-29 17:37:42 +0700

    test function slugify of JoBeet class

---
+++ src/JoBeetBundle/Tests/Utils/JoBeetTest.php
@@ -0,0 +1,18 @@
+<?php
+// src/JoBeetBundle/Tests/Utils/JoBeetTest.php
+
+namespace JoBeetBundle\Tests\Utils;
+use JoBeetBundle\Utils\JoBeet;
+
+class JoBeetTest extends \PHPUnit_Framework_TestCase
+{
+  public function testSlugify()
+  {
+    $this->assertEquals('sensio', Jobeet::slugify('Sensio'));
+    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio labs'));
+    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio   labs'));
+    $this->assertEquals('paris-france', Jobeet::slugify('paris,france'));
+    $this->assertEquals('sensio', Jobeet::slugify('  sensio'));
+    $this->assertEquals('sensio', Jobeet::slugify('sensio  '));
+  }
+}

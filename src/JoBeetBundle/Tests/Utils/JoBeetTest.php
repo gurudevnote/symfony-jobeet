54dddc36c26db50b2f8da6de09c9c38579313273
Huy Nguyen Huu <huuhuy@gmail.com>
2015-12-29 17:52:30 +0700

    Improve test unit

--- src/JoBeetBundle/Tests/Utils/JoBeetTest.php
+++ src/JoBeetBundle/Tests/Utils/JoBeetTest.php
@@ -8,13 +8,16 @@ class JoBeetTest extends \PHPUnit_Framework_TestCase
 {
   public function testSlugify()
   {
-    $this->assertEquals('sensio', Jobeet::slugify('Sensio'));
-    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio labs'));
-    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio   labs'));
-    $this->assertEquals('paris-france', Jobeet::slugify('paris,france'));
-    $this->assertEquals('sensio', Jobeet::slugify('  sensio'));
-    $this->assertEquals('sensio', Jobeet::slugify('sensio  '));
-    $this->assertEquals('n-a', Jobeet::slugify(''));
-    $this->assertEquals('developpeur-web', Jobeet::slugify('Développeur Web'));
+    $this->assertEquals('sensio', JoBeet::slugify('Sensio'));
+    $this->assertEquals('sensio-labs', JoBeet::slugify('sensio labs'));
+    $this->assertEquals('sensio-labs', JoBeet::slugify('sensio   labs'));
+    $this->assertEquals('paris-france', JoBeet::slugify('paris,france'));
+    $this->assertEquals('sensio', JoBeet::slugify('  sensio'));
+    $this->assertEquals('sensio', JoBeet::slugify('sensio  '));
+    $this->assertEquals('n-a', JoBeet::slugify(''));
+    if (function_exists('iconv'))
+    {
+      $this->assertEquals('developpeur-web', JoBeet::slugify('Développeur Web'));
+    }
   }
 }

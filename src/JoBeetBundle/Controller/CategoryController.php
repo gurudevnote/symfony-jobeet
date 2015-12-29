621ac8d8811f8ec218f98b3c5437da2594068187
Huy Nguyen Huu <huuhuy@gmail.com>
2015-12-29 16:35:38 +0700

    Implement pagination for category page

--- src/JoBeetBundle/Controller/CategoryController.php
+++ src/JoBeetBundle/Controller/CategoryController.php
@@ -14,15 +14,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class CategoryController extends Controller
 {
     /**
-     * @Route(name="job_category", path="/{slug}")
+     * @Route(name="job_category", path="/{slug}/{page}")
      */
-    public function showAction(Category $category)
+    public function showAction(Category $category, $page = 1)
     {
         $em = $this->getDoctrine()->getManager();
         $category->setActiveJobs($em->getRepository('JoBeetBundle:Job')->getActiveJobs($category->getId()));
 
+        $total_jobs = $em->getRepository('JoBeetBundle:Job')->countActiveJobs($category->getId());
+        $jobs_per_page = $this->container->getParameter('max_jobs_on_category');
+        $last_page = ceil($total_jobs / $jobs_per_page);
+        $previous_page = $page > 1 ? $page - 1 : 1;
+        $next_page = $page < $last_page ? $page + 1 : $last_page;
+
+        $category->setActiveJobs($em->getRepository('JoBeetBundle:Job')->getActiveJobs($category->getId(), $jobs_per_page, ($page - 1) * $jobs_per_page));
+
         return $this->render(':category:show.html.twig', array(
             'category' => $category,
+            'last_page' => $last_page,
+            'previous_page' => $previous_page,
+            'current_page' => $page,
+            'next_page' => $next_page,
+            'total_jobs' => $total_jobs
         ));
     }
 }

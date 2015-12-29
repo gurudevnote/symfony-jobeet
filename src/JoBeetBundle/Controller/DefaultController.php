954e24d5a599e54c58f2c42eff6ad69286b21b05
Huy Nguyen Huu <huuhuy@gmail.com>
2015-12-29 17:15:58 +0700

    Add new bundle of phpunit, using to test "vendor\bin\phpunit -c ."

--- src/JoBeetBundle/Controller/DefaultController.php
+++ src/JoBeetBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JoBeetBundle:Default:index.html.twig');
+        return $this->redirectToRoute('job_index');
     }
 }

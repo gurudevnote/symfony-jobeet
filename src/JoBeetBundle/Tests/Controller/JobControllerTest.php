58fbf389ef15df70ea5b3bdcb4419644a279c730
Huy Nguyen Huu <huuhuy@gmail.com>
2015-12-30 17:08:58 +0700

    Test Only n jobs are listed for a category via command "vendor\bin\phpunit"

--- src/JoBeetBundle/Tests/Controller/JobControllerTest.php
+++ src/JoBeetBundle/Tests/Controller/JobControllerTest.php
@@ -15,5 +15,10 @@ class JobControllerTest extends WebTestCase
         $this->assertEquals('JoBeetBundle\Controller\JobController::indexAction', $client->getRequest()->attributes->get('_controller'));
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
         $this->assertTrue($crawler->filter('.jobs td.position:contains("Expired")')->count() == 0);
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $max_jobs_on_homepage = $kernel->getContainer()->getParameter('max_jobs_on_homepage');
+        $this->assertLessThanOrEqual($crawler->filter('.category_programming tr')->count(), $max_jobs_on_homepage);
     }
 }

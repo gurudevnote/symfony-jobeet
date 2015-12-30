e8dcbad293faa704b6cc2e43f44a8fecf365eeb5
Huy Nguyen Huu <huuhuy@gmail.com>
2015-12-30 17:17:26 +0700

    reflextor code

--- src/JoBeetBundle/Tests/Controller/JobControllerTest.php
+++ src/JoBeetBundle/Tests/Controller/JobControllerTest.php
@@ -25,14 +25,21 @@ class JobControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
         $this->assertTrue($crawler->filter('.category_administrator .more_jobs')->count() == 1);
 
+        $job = $this->getMostRecentProgrammingJob();
+
+        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $job->getId()))->count() == 1);
+    }
+
+    public function getMostRecentProgrammingJob()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
         $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 
         $query = $em->createQuery('SELECT j from JoBeetBundle:Job j LEFT JOIN j.category c WHERE c.slug = :slug AND j.expires_at > :date ORDER BY j.created_at DESC');
         $query->setParameter('slug', 'programming');
         $query->setParameter('date', date('Y-m-d H:i:s', time()));
         $query->setMaxResults(1);
-        $job = $query->getSingleResult();
-
-        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $job->getId()))->count() == 1);
+        return $query->getSingleResult();
     }
 }
